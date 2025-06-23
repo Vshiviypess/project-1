753a7307687e921065c6acb97cfb2d0502966972
Vshiviypess <vshiviypess228@gmail.com>
2025-06-23 11:22:30 +0300

    Add files via upload

---
+++ ToDo.Entities/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToDo.Entities
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
---
+++ UserRepos/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows;
+using System.Net.Mail;
+using System.Globalization;
+
+
+
